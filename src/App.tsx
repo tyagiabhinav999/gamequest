36b4ff1a80e8edeea49a10e5985d864e50e28a7a
Abhinav Tyagi <AbhinavTyagi@bdo.in>
2023-06-15 16:09:02 +0530

    Implement Navigation Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import { Button } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
-return (<Button colorScheme='teal' size={'lg'}>Button</Button>);
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"}>
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"}>
+        </GridItem>
+      </Show>
+      <GridItem area={"main"}>
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
